c0f7bf5db21fcb90ba1fca1fd3163a462f2412dc
Ishan Subedi <ikushum@gmail.com>
2018-06-14 17:06:51 +0545

    add login required decorator

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
 import json
 
+@login_required
 def index(request):
     return render(request, 'chat/index.html', {})
 
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
