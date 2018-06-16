539320d86142d8780726ed1c4e47d05e24a6107a
Ishan Subedi <ikushum@gmail.com>
2018-06-16 11:59:19 +0545

    change consumer name

--- chat/groupChatConsumers.py
+++ chat/groupChatConsumers.py
@@ -2,7 +2,7 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
-class ChatConsumer(WebsocketConsumer):
+class GroupChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
