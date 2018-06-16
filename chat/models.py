624dfc4be90f40a853919722ff9ac8f2fe5b9f88
Ishan Subedi <ikushum@gmail.com>
2018-06-17 01:17:37 +0545

    create Message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, related_name='messages_sent', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='messages_received', on_delete=models.CASCADE)
